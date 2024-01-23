43f238b990c3e9604ce6f0558cec4bdaf33241cb
nuburooj <nuburoojkhattak@gmail.com>
2024-01-23 00:34:09 -0500

    fixed persistence issue and Util dependancy error for unique id package

--- contact-manager/src/components/App.js
+++ contact-manager/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { v4 as idGnr } from 'uuid'
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList'
@@ -10,20 +11,36 @@ function App() {
 
   const addContactHandler = (contact) => {
     // console.log(contact);
-    setContacts([...contacts, contact])
+
+    // we are accessing the contacts already present in local storage
+    let exsContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    // we are setting the updated contacts array in which new contacts will be added
+    let updatedContacts=[]
+    // if there are contacts present, then add contacts and unique ids to updated contacts array
+    if(exsContacts.length>0){
+      updatedContacts = [...exsContacts, { id: idGnr(), ...contact }]
+      // if there are no contacts, make this the first one
+    }else{
+      updatedContacts = [{ id: idGnr(), ...contact }]
+    }
+    
+     
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updatedContacts));
+    setContacts(updatedContacts)
   }
 
   useEffect(() => {
     console.log("useEffect - Retrieving data from local storage");
 
     const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  console.log(retrieveContacts)
     if (retrieveContacts) setContacts(retrieveContacts);
   }, []);
 
-  useEffect(() => {
-    console.log("useEffect - Storing data in local storage");
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  },[contacts]);
+  // useEffect(() => {
+  //   console.log("useEffect - Storing data in local storage");
+  //  // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  // },[contacts]);
 
   return (
     <div className="ui container">
