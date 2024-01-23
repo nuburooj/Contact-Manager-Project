d057d8872f8d1b6a7aa87c1d99bbc7bbe2b37ba6
nuburooj <nuburoojkhattak@gmail.com>
2024-01-23 16:04:27 -0500

    logged the data in the console to see if the right info is being accessed

--- contact-manager/src/components/App.js
+++ contact-manager/src/components/App.js
@@ -28,6 +28,7 @@ function App() {
     
     const response = await api.post("/contacts", request)
     setContacts([...contacts, response.data])
+    console.log(response)
     // we are accessing the contacts already present in local storage
     // let exsContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     // we are setting the updated contacts array in which new contacts will be added
