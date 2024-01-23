49aa7095569ead69635b53c4f5e0ebf10b9fbc9f
nuburooj <nuburoojkhattak@gmail.com>
2024-01-22 20:59:41 -0500

    Made the contacts persist after being stored in local storage

--- contact-manager/src/components/App.js
+++ contact-manager/src/components/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList'
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'contacts'
   const [contacts, setContacts] = useState([])
 
   const addContactHandler = (contact) => {
-    console.log(contact);
+    // console.log(contact);
     setContacts([...contacts, contact])
   }
+
+  useEffect(() => {
+    console.log("useEffect - Retrieving data from local storage");
+
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect - Storing data in local storage");
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
+
   return (
     <div className="ui container">
       <Header/>
