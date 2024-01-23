2206608aea5595d94e6428ec768f59ea485c6bc1
nuburooj <nuburoojkhattak@gmail.com>
2024-01-22 20:16:02 -0500

    created state for contacts and managed to console.log the results from the OnSubmit and Onchange listeners in the console after submitting the form

--- contact-manager/src/components/App.js
+++ contact-manager/src/components/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList'
 
 function App() {
-
-  const contacts = [
-    {
-      id: "1",
-      name: "Nuburooj",
-      email:"nuburooj@gmail.com"
-    },
-    {
-      id: "2",
-      name: "Tyler",
-      email: "Tyler@gmail.com"
-    },
-  ];
+  const [contacts, setContacts] = useState([])
   return (
     <div className="ui container">
       <Header/>
