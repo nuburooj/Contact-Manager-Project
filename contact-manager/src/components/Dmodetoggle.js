10587551bd00142bb0125aa2354542afda9d3330
TylerCharlton <TyCharlton1993@gmail.com>
2024-01-24 13:19:51 -0500

    changed the button into a toggle slider still needs tweaking but it functionality is there

--- contact-manager/src/components/Dmodetoggle.js
+++ contact-manager/src/components/Dmodetoggle.js
@@ -3,16 +3,17 @@ import React, { useState, useEffect } from 'react';
 export const Dmodetoggle = () => {
   const [isDark, setIsDarkMode] = useState(false);
 
-  
+
   useEffect(() => {
     document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
   }, [isDark]);
 
   return (
-    <div>
-      <button onClick={() => setIsDarkMode(!isDark)}>
-        {isDark ? 'Light Mode' : 'Dark Mode'}
-      </button>
+    <div className="dark-mode-toggle">
+      <label className="switch">
+        <input type="checkbox" onChange={() => setIsDarkMode(!isDark)} checked={isDark} />
+        <span className="slider round"></span>
+      </label>
     </div>
   );
 };
