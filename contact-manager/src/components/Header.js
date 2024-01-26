a4fde8664d4ab909a72cf8ca15166f8aa2f34d20
nuburooj <nuburoojkhattak@gmail.com>
2024-01-25 21:01:47 -0500

    more css tweaks and updated db

--- contact-manager/src/components/Header.js
+++ contact-manager/src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Dmodetoggle } from './Dmodetoggle';
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
-    <div className="ui container center">
-      <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-        <h2 className="contact-manager-heading">Contact Manager</h2>
-      </Link>
-        <Dmodetoggle /> 
+      <div className="ui container center">
+        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <h2 className="contact-manager-heading">Contact Manager</h2>
+        </Link>
+        <Dmodetoggle />
       </div>
     </div>
   );
